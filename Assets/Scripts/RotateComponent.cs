93db237ae816396d2365a66fefa6e06688f651bc
precabal <pablorecabal@gmail.com>
2015-05-22 08:08:00 -0400

    added offset parameter in progress...

--- Assets/Scripts/RotateComponent.cs
+++ Assets/Scripts/RotateComponent.cs
@@ -7,29 +7,37 @@ public class RotateComponent : MonoBehaviour
 	public Vector3 _rotationAngle;
 	public  float _rotationTime;
 	public float _rotationPeriod;
+	public float _percentageOffset;
 
 	private Vector3 _rotationStep;
 	private int _rotationCount;
 	private int _numberOfSteps;
+	private int _offsetInSteps;
 
 	private float _timer;
+
+
 
 	
 	void Start ()
 	{
 		//TODO: force that _rotationTime to be a multiple of Time.fixedDeltaTime
+		//if (Math.IEEERemainder(_rotationTime,Time.fixedDeltaTime) > 0)
+
 		_numberOfSteps = (int)Math.Floor(_rotationTime / Time.fixedDeltaTime);
+		_offsetInSteps = (int)Math.Floor (_percentageOffset * (float)_numberOfSteps);
 		_rotationStep = _rotationAngle / _numberOfSteps;
 		_timer = 0f;
 	}
 	
 	void FixedUpdate ()
 	{
-		if (_timer > _rotationPeriod) {
+
+		if (_timer >= _rotationPeriod) {
 			_rotationCount = 0;
 			_timer = 0;
 		}
-		if (_rotationCount < _numberOfSteps)
+		if ( (0 <= _rotationCount - _offsetInSteps) && (_rotationCount - _offsetInSteps < _numberOfSteps) )
 		{
 			this.transform.Rotate (_rotationStep);
 			_rotationCount++;
--- Assets/Scripts/RotateEvent.cs
+++ Assets/Scripts/RotateEvent.cs
@@ -10,34 +10,37 @@ namespace AssemblyCSharp
 		private Vector3 _rotationAngle;
 		private float _rotationTime;
 		private float _rotationPeriod;
+		private float _percentageOffset;
 
 		public float EventTime
 		{
 			get { return _eventTime; }
 			set { _eventTime = value; }
 		}
-		public RotateEvent (float eventTime, Vector3 rotationAngle, float rotationTime, float rotationPeriod)
+		public RotateEvent (float eventTime, Vector3 rotationAngle, float rotationTime, float rotationPeriod, float percentageOffset = 0f)
 		{
 			_eventTime = eventTime;
 			_rotationAngle = rotationAngle;
 			_rotationTime = rotationTime;
 			_rotationPeriod = rotationPeriod;
+			_percentageOffset = percentageOffset;
 		}
 
 		public void Perform(List<GameObject> objects)
 		{
 			foreach (GameObject obj in objects)
 			{
-				CreateComponent(obj, _rotationAngle, _rotationTime, _rotationPeriod);
+				CreateComponent(obj, _rotationAngle, _rotationTime, _rotationPeriod, _percentageOffset);
 			}
 		}
 
-		private void CreateComponent(GameObject target, Vector3 rotationAngle, float rotationTime, float rotationPeriod)
+		private void CreateComponent(GameObject target, Vector3 rotationAngle, float rotationTime, float rotationPeriod, float percentageOffset)
 		{
 			RotateComponent rotateComponent = target.AddComponent<RotateComponent> ();
 			rotateComponent._rotationAngle = rotationAngle;
 			rotateComponent._rotationTime = rotationTime;
 			rotateComponent._rotationPeriod = rotationPeriod;
+			rotateComponent._percentageOffset = percentageOffset;
 
 		}
 	}
